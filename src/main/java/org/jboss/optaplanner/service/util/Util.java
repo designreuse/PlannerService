62529ee590444cc9e2f2949a57731150c670a189
Martin Večeřa <mvecera@redhat.com>
2014-06-30 18:05:26 +0200

    added ability to solve more problems

--- src/main/java/org/jboss/optaplanner/service/util/Util.java
+++ src/main/java/org/jboss/optaplanner/service/util/Util.java
@@ -6,6 +6,9 @@ import com.thoughtworks.xstream.converters.reflection.NativeFieldKeySorter;
 import com.thoughtworks.xstream.converters.reflection.PureJavaReflectionProvider;
 import org.optaplanner.core.impl.solution.Solution;
 import org.optaplanner.examples.nqueens.domain.NQueens;
+import org.optaplanner.examples.cloudbalancing.domain.CloudBalance;
+import org.optaplanner.examples.projectjobscheduling.domain.Schedule;
+import org.optaplanner.examples.travelingtournament.domain.TravelingTournament;
 
 import java.io.*;
 
@@ -15,52 +18,52 @@ import java.io.*;
 @SuppressWarnings("rawtypes")
 public class Util {
 
-  public static void toXStream(Solution sol, File f) {
-    XStream xs = getXStream();
-    try {
-      xs.toXML(sol, new FileWriter(f));
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
+	public static void toXStream(Solution sol, File f) {
+		XStream xs = getXStream();
+		try {
+			xs.toXML(sol, new FileWriter(f));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-  public static void toXStream(Solution sol, OutputStreamWriter osw) {
-    XStream xs = getXStream();
-    xs.toXML(sol, osw);
-  }
+	public static void toXStream(Solution sol, OutputStreamWriter osw) {
+		XStream xs = getXStream();
+		xs.toXML(sol, osw);
+	}
 
-  public static String toXml(Solution sol) {
-    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    try (OutputStreamWriter osw = new OutputStreamWriter(baos, "UTF-8")) {
-      toXStream(sol, osw);
-      return baos.toString("UTF-8");
-    } catch (IOException e) {
-      throw new IllegalStateException("UTF-8 should have been supported: ", e);
-    }
-  }
+	public static String toXml(Solution sol) {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		try (OutputStreamWriter osw = new OutputStreamWriter(baos, "UTF-8")) {
+			toXStream(sol, osw);
+			return baos.toString("UTF-8");
+		} catch (IOException e) {
+			throw new IllegalStateException("UTF-8 should have been supported: ", e);
+		}
+	}
 
-  public static Solution fromXml(String xml) {
-    try {
-      ByteArrayInputStream bais = new ByteArrayInputStream(xml.getBytes());
-      return fromXStream(new InputStreamReader(bais, "UTF-8"));
-    } catch (UnsupportedEncodingException e) {
-      throw new IllegalStateException("UTF-8 should have been supported: ", e);      
-    }
-  }
+	public static Solution fromXml(String xml) {
+		try {
+			ByteArrayInputStream bais = new ByteArrayInputStream(xml.getBytes());
+			return fromXStream(new InputStreamReader(bais, "UTF-8"));
+		} catch (UnsupportedEncodingException e) {
+			throw new IllegalStateException("UTF-8 should have been supported: ", e);      
+		}
+	}
 
-  private static XStream getXStream() {
-    XStream xs = new XStream();
-    xs.setMode(XStream.ID_REFERENCES);
-    xs.processAnnotations(NQueens.class);
-/*      xs.processAnnotations(Column.class);
-      xs.processAnnotations(Queen.class);
-      xs.processAnnotations(Row.class);*/
-    return xs;
-  }
+	private static XStream getXStream() {
+		XStream xs = new XStream();
+		xs.setMode(XStream.ID_REFERENCES);
+		xs.processAnnotations(NQueens.class);
+		xs.processAnnotations(CloudBalance.class);
+		xs.processAnnotations(Schedule.class);
+		xs.processAnnotations(TravelingTournament.class);
+		return xs;
+	}
 
-  public static Solution fromXStream(InputStreamReader r) {
-    return (Solution) getXStream().fromXML(r);
+	public static Solution fromXStream(InputStreamReader r) {
+		return (Solution) getXStream().fromXML(r);
 
-  }
+	}
 
 }
